30cb25846c816d004196831e3a67d52fcdb67509
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-05-05 19:57:11 -0700

    Real Animal, Config, and World classes and tests

--- pi.py
+++ pi.py
@@ -1,3 +1,45 @@
+#!/usr/bin/python3
+
+import random
+import math
+
 class Animal():
+    def __init__(self, numerator=None, denominator=None):
+        self.max_int = 2147483647
+        self.numerator = random.randint(1, self.max_int)
+        self.denominator = random.randint(1, self.max_int)
+        self.age = 0
+        self.fitness = abs(math.pi - self.get_pi())  # distance from pi
+
+    def get_pi(self):
+        return self.numerator / (self.denominator * 1.0)
+
+class Config():
     def __init__(self):
-        self.numerator = 1
+        self.max_age = 100
+        self.max_generations = 4
+        self.sleep_time_ms = 1000
+        self.max_population = 50
+        self.initial_population = 10
+        self.max_distance_from_pi = 314159
+        self.mutation_percentage = 0.02
+
+    def __str__(self):
+        return 'World configuration: ' + str(self.__dict__) + '\n'
+
+class World():
+    def __init__(self, config):
+        self.animals = []
+        self.fill_world(config.initial_population)
+
+    def fill_world(self, population):
+        while len(self.animals) < population:
+            animal = Animal()
+            self.animals.append(animal)
+
+
+
+if __name__ == "__main__":
+    config = Config()
+    print(config)
+
--- test_pi.py
+++ test_pi.py
@@ -7,4 +7,28 @@ class TestPi(unittest.TestCase):
     def test_bare_init(self):
         animal = pi.Animal()
         self.assertTrue(isinstance(animal.numerator, int))
+        self.assertTrue(isinstance(animal.denominator, int))
+        self.assertTrue(isinstance(animal.age, int))
+        self.assertTrue(isinstance(animal.fitness, float))
+
+    def test_get_pi_datatype(self):
+        animal = pi.Animal()
+        self.assertTrue(not float.is_integer(animal.get_pi()))
+
+    def test_animal_optional_params(self):
+        animal = pi.Animal(3, 6)
+
+    def test_animal_age_starts_at_zero(self):
+        animal = pi.Animal()
+        self.assertEqual(0, animal.age)
+
+    def test_config_print(self):
+        config = pi.Config()
+        assert('World configuration: {' in config.__str__())
+
+    def test_initial_population_count(self):
+        config = pi.Config()
+        world = pi.World(config)
+        self.assertEqual(len(world.animals), config.initial_population)
+
 
